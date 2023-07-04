d8d88ba7089332415fc9322814507e97a4597f1f
Thibault Sulem <thibault.sulem@edu.devinci.fr>
2023-07-04 11:57:38 +0200

    ajout du test unitaire

---
+++ index.test.js
@@ -0,0 +1,8 @@
+const getRandomNumber = require('../iim-td4/index');
+
+function main() {
+    const randomNumber = getRandomNumber();
+    console.log('Random Number:', randomNumber);
+  }
+  
+  main();
